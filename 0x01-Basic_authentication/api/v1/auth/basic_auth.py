955c004729dc22018606ea07446d228fd92d69c5
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2024-07-09 13:56:30 +0000

    feat: decode a base64-encoded authorization header

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,6 +5,8 @@ This module provides the BasicAuth class which is a subclass of Auth.
 It handles basic authentication for the API.
 """
 
+import base64
+import binascii
 from api.v1.auth.auth import Auth
 
 
@@ -31,3 +33,30 @@ class BasicAuth(Auth):
         if not authorization_header.startswith(prefix):
             return None
         return authorization_header[len(prefix):]
+
+    def decode_base64_authorization_header(
+            self,
+            base64_authorization_header: str) -> str:
+        """
+        Decodes a base64-encoded authorization header.
+
+        Args:
+            base64_authorization_header (str): The base64-encoded
+                authorization header.
+
+        Returns:
+            str: The decoded authorization header as a string.
+
+        Raises:
+            None
+        """
+        if not isinstance(base64_authorization_header, str):
+            return None
+
+        try:
+            decoded_base64 = base64.b64decode(
+                base64_authorization_header,
+                validate=True)
+            return decoded_base64.decode('utf-8')
+        except binascii.Error:
+            return None
