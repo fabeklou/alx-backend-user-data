66521ebb3f73de0de9ec3c1a39e7c031a9099ee9
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2024-07-10 01:02:48 +0000

    fix: stars checking logic

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -42,8 +42,19 @@ class Auth:
         if len(path) > 0 and path[-1] != "/":
             path = path + '/'
 
+        _excluded_stars_paths = []
+        _excluded_paths = []
         for excluded_path in excluded_paths:
-            if excluded_path != '*' and re.match(excluded_path, path):
+            if len(excluded_path) > 0 and excluded_path[-1] == '*':
+                _excluded_stars_paths.append(excluded_path[:-1])
+            else:
+                _excluded_paths.append(excluded_path)
+
+        for p in _excluded_stars_paths:
+            if path.startswith(p):
+                return False
+        for p in _excluded_paths:
+            if p == path:
                 return False
         return True
 
