630440130b521215cc836a8c27edd4af1154e17f
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2024-07-10 18:20:55 +0000

    fix: set current_user only if user logged-in

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -45,18 +45,18 @@ def before_request():
     paths = ['/api/v1/status/', '/api/v1/unauthorized/',
              '/api/v1/forbidden/', '/api/v1/auth_session/login/']
 
-    request.current_user = None
     if auth is None:
         return
-
-    request.current_user = auth.current_user(request)
 
     if auth.require_auth(request.path, paths):
         if auth.authorization_header(request) is None and\
                 auth.session_cookie(request) is None:
             abort(401)
+
         if auth.current_user(request) is None:
             abort(403)
+
+        request.current_user = auth.current_user(request)
 
 
 @app.errorhandler(404)
