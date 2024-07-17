2541afddcfaebe5ba84bb0a2c87def704b177ad1
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2024-07-17 11:49:41 +0000

    feat: create PUT /reset_password route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -174,5 +174,32 @@ def get_reset_password_token():
         return jsonify({'email': email, 'reset_token': reset_token}), 200
 
 
+@app.route('/reset_password', methods=['PUT'])
+def update_password():
+    """
+    Updates the password for a user.
+
+    Returns:
+        A JSON response containing the email and a success message.
+
+    Raises:
+        403: If any of the required parameters are missing or if
+            the reset token is invalid.
+    """
+    email = request.form.get('email')
+    reset_token = request.form.get('reset_token')
+    new_password = request.form.get('new_password')
+
+    if email is None or reset_token is None or new_password is None:
+        abort(403)
+
+    try:
+        AUTH.update_password(reset_token, new_password)
+    except ValueError:
+        abort(403)
+    else:
+        return jsonify({'email': email, 'message': 'Password updated'}), 200
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
