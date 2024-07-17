5f017e73427065145c82570eb5f8e47c9fcba470
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2024-07-17 11:07:31 +0000

    feat: generate a reset password token

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -145,3 +145,25 @@ class Auth:
         """
         if isinstance(user_id, int):
             self._db.update_user(user_id, session_id=None)
+
+    def get_reset_password_token(self, email: str) -> str:
+        """
+        Retrieves the reset password token for a given email.
+
+        Args:
+            email (str): The email of the user.
+
+        Returns:
+            str: The reset password token.
+
+        Raises:
+            ValueError: If no user is found with the given email.
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            raise ValueError
+        else:
+            reset_token_uuid = _generate_uuid()
+            self._db.update_user(user.id, reset_token=reset_token_uuid)
+            return reset_token_uuid
